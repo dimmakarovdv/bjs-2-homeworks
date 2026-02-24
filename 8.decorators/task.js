c7b1b7174295b8ff56aa724f7069a8ce9369c02a
Neto test <dimmakarovdv89@mail.ru>
2026-02-24 21:48:40 +0500

    Solution to task 8

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,53 @@
-//Задача № 1
 function cachingDecoratorNew(func) {
-  
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = md5(args);
+    const objectInCache = cache.find((item) => item.hash === hash);
+
+    if (objectInCache) {
+      console.log("Из кеша: " + objectInCache.value);
+      return "Из кеша: " + objectInCache.value;
+    }
+
+    const result = func(...args);
+    cache.push({ hash, value: result });
+
+    if (cache.length > 5) {
+      cache.shift();
+    }
+
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
+
+  return wrapper;
 }
 
-//Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+  let timeoutId = null;
+  let firstCallExecuted = false;
+
+  function wrapper(...args) {
+    wrapper.allCount++;
+
+    if (!firstCallExecuted && !timeoutId) {
+      func.apply(this, args);
+      wrapper.count++;
+      firstCallExecuted = true;
+    } else {
+      clearTimeout(timeoutId);
+    }
+
+    timeoutId = setTimeout(() => {
+      func.apply(this, args);
+      wrapper.count++;
+      timeoutId = null;
+    }, delay);
+  }
+
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+
+  return wrapper;
 }
